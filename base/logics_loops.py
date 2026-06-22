6e98c8cf2ea437d73d52f3754a0594395f97e772
co11ection <tima.j.zh@gmail.com>
2026-06-22 18:42:01 +0600

    logics_loops

--- base/logics_loops.py
+++ base/logics_loops.py
@@ -122,3 +122,185 @@ elif user_number < 0:
     print("Число отрицательное")
 else:
     print("Число равно нулю")
+    
+# Д/З
+# написать калькулятор
+
+
+
+# ================== Циклы =====================
+print("Привет")
+print("Привет")
+print("Привет")
+print("Привет")
+print("Привет")
+print("Привет")
+print("Привет")
+print("Привет")
+# более 200
+# Цикл - Повторение одного и того же кода многократно
+# Цикл делится на 2 вида: for, while
+
+
+# ============= for ==============
+# Цикл for проходит по итерирууемым обьектам или типам данных(
+# list, dict, set, str, tuple   
+#)
+# Итерация - повторение
+
+for i in range(1, 201):
+    print(f"{i} Привет!")
+    
+    
+# Вывести числа от 1 до 10
+
+for i in range(1, 11):
+    print(i)
+    
+
+# вывести числа от 10 до 1
+
+for i in range(10, 0, -1):
+    print(i)
+    
+# Вывести только четные числа от 1 до 20 включительно
+print("!!!!!!!!!!!")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+        
+# Перебор строки
+word = "Python"
+for letter in word:
+    print(letter)
+    
+# вывести по буквам слово "Программирование"
+word = "Программирование"
+for letter in word:
+    print(letter)
+
+# Перебор списка
+fruits = ['apple', 'banan', 'grape', 'pear']
+for fruit in fruits:
+    print(fruit)
+    
+
+animals = [
+    'кот',
+    'собака',
+    'хомяк',
+    'лев'
+]
+
+for animal in animals:
+    print(animal)
+    
+
+# Сумма чисел от 1 до 5
+total = 0
+# total = total + num
+# total += num
+for num in range(1, 6):
+    total += num
+    print(total)
+    
+# найти сумму всех четных чисел от 1 до 50
+total = 0
+for num in range(1, 51):
+    if num % 2 == 0:
+        total += num
+        
+print(total)
+
+# ================ While =============
+# бесконечный цикл
+# Синтаксис
+# while <условие>:
+#     действие
+
+num = 1
+
+while num <= 10:
+    print(num)
+    num += 1
+    
+
+# вывести числа от 1 до 20
+
+num = 1
+
+while num <= 20:
+    print(num)
+    num += 1
+
+
+
+# выести числа от 20 до 1
+
+num = 20
+
+while num != 0:
+    print(num)
+    num -= 1
+    
+password = ''
+
+# while password != 'python':
+#     password = input("Введите пароль: ")
+    
+# print("Вы вошли в систему")
+
+
+# Пользователь вводит число пока не введет 0
+
+user_number = None
+
+while user_number != 0:
+    user_number = int(input("Введите число: "))
+    
+
+# Оператор break
+# оставливает цикл
+
+for i in range(10):
+    if i == 5:
+        break
+    else:
+        print(i) 
+        
+
+# Оператор contiune
+# Пропускает одну итерацию
+
+for i in range(1, 10):
+    if i == 3:
+        continue
+    else:
+        print(i)
+
+
+# вывести число 1 до 20 кроме 5, 10, 15
+for i in range(1,21):
+    if i == 5 or i == 10 or i == 15:
+        continue
+    else:
+        print(i)
+
+print('!!!!!!!!!!')
+
+for i in range(1, 21):
+    if i in [5, 10, 15]:
+        continue
+    else:
+        print(i)
+        
+        
+#НЕ ДЕЛАЙ ТАК
+# while True:
+#     print(1)
+
+
+# list1 = [1, 2, 3, 4]
+# for i in list1:
+#     list1.append(i)
+#     print(list1)
